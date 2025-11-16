4adefb2ebfecf1c76d45f410f24d8660d4dc4a80
José Ameixa <himynameisplum@gmail.com>
2025-11-16 15:22:09 +0000

    Update wsgi.py

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -2,4 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.config.settings")
-application = get_wsgi_application()
+application = get_wsgi_application
